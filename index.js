64583f50a6b8ddf69a650194405cdfbdbecb417c
unknown <e.fraffo@gmail.com>
2020-05-24 17:34:38 -0300

    24/05

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
-const http = require('http');
-const util = require('util');
+const express = require('express');
+const cors = require('cors');
 
 // https://github.com/node-formidable/node-formidable
-const Formidable = require('formidable');
+const Formidable = require('formidable'); 
+const util = require('util');
 
 //https://www.npmjs.com/package/dotenv
 const cloudinary = require("cloudinary");
 require('dotenv').config()
 
+const app = express();
+app.use(cors());
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
 });
-
-
-//Create a server
-http.createServer((req, res) => {
-    if (req.url === '/upload' && req.method.toLowerCase() === 'post') {
-
-        // parse a file upload
-        const form = new Formidable();
-
-        form.parse(req, (err, fields, files) => {
-
-            //https://cloudinary.com/documentation/upload_images
-            cloudinary.uploader.upload(files.upload.path, result => {
-
-                console.log(result)
-                if (result.public_id) {
-                    res.writeHead(200, { 'content-type': 'text/plain' });
-                    res.write('received upload:\n\n');
-                    res.end(util.inspect({ fields: fields, files: files }));
-                }
-            });
-        });
-        return;
-    }
-
-    // show a file upload form
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.end(`
-    <form action="/upload" enctype="multipart/form-data" method="post">
+ 
+app.get('/', (req, res) => {
+  res.send(`
+    <form action="/api/upload" enctype="multipart/form-data" method="post">
       <input type="text" name="title" /><br/>
-      <input type="file" name="upload" multiple="multiple" /><br/>
+      <input type="file" name="upload" /><br/>
       <input type="submit" value="Upload" />
     </form>
   `);
+});
+ 
+app.post('/api/upload', (req, res, next) => {
 
-}).listen(8080);
+  const form = new Formidable();
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    console.log('files: ', files)
+    //https://cloudinary.com/documentation/upload_images
+    cloudinary.uploader.upload(files.upload.path, result => {
+
+      console.log('result: ', result) // en el result está la url de la imagen 
+      if (result.public_id) {
+          res.writeHead(200, { 'content-type': 'text/plain' });
+          res.write('received upload:\n\n');
+          res.end(util.inspect({ fields: fields, files: files, root: result.secure_url }));
+      }
+    });
+  });
+});
+ 
+app.listen(3000, () => {
+  console.log('Server listening on http://localhost:3000 ...');
+});
--- indexJQ.js
+++ indexJQ.js
@@ -1,91 +1,42 @@
-// $(document).ready(function() {
-//     if($.fn.cloudinary_fileupload !== undefined) {
-//       $("input.cloudinary-fileupload[type=file]").cloudinary_fileupload();
-//     }
-// });
-
-$(function() {
-    if($.fn.cloudinary_fileupload !== undefined) {
-        $("input.cloudinary-fileupload[type=file]").cloudinary_fileupload();
-        console.log($("input.cloudinary-fileupload[type=file]"))
-    }
+$('#image').change((event) => {
+    var formData = new FormData($('#form')[0]);
+    renderImage(formData);
 });
 
-// $(document).ready(function() {
-//     $('.cloudinary-fileupload').cloudinary_fileupload({
-//       disableImageResize: false,
-//       imageMaxWidth: 800,                   // 800 is an example value - no default
-//       imageMaxHeight: 600,                  // 600 is an example value - no default
-//       maxFileSize: 20000000,                // 20 MB is an example value - no default
-//       loadImageMaxFileSize: 20000000,       // default is 10 MB
-//       acceptFileTypes: /(\.|\/)(gif|jpe?g|png|bmp|ico)$/i
-//     });
-//   });
+function renderImage(formData){
+    const file = formData.get('upload');
+    const image = URL.createObjectURL(file);
+    $('.card-img-top').attr('src', image);
+}
 
-
-$('form').submit(function(event){
-    // event.preventDefault();
-    // alert('Enviado');
-    var server = 'http://localhost:8080';
+$(".upload").click(function(event) {
+    event.preventDefault();
+    var server = 'http://localhost:3000';
+    // var file = $('#image')[0].files[0];
+    var formData = new FormData($('#form')[0]);
+    
     $.post({
-        url: server + "/upload",
-        success: function(){
-            alert('Imagen subida');
-            // window.location.href = "index.html";
+        url: server + "/api/upload",
+        data: formData,
+        contentType: false,
+        processData: false,
+        beforeSend: function() {
+			$('#enviando').prepend('<img src="images/Spinner-1s-200px.gif" />');
+        },
+        complete: function(){
+            $('#enviando').empty();
+            $('#enviando').prepend('<h1> Imagen subida </h1>');
+        },
+        success: function(response) {
+            console.log(response)
+            if (response != 0) {
+                $(".card-img-top").attr("src", response.root);
+            } else {
+                alert('Formato de imagen incorrecto.');
+            }
         },
         error: function(error){
-            alert('Error al dubir la imagen')
-            // window.location.href = "error.html";
-        }
+            console.log(error);
+        },
     });
 });
-
-// Al finalizarse de cargar el DOM:
-// $(function() {
-// 	// Se obtiene el id en la URL usando la función en helpers.js
-// 	var idCompetencia = getQueryParam("id");
-// 	// Se obtienen del backend y cargan en el DOM los detalles de la competencia
-// 	var competenciasController = new CompetenciasController();
-// 		competenciasController.obtenerCompetencia(idCompetencia)
-// 	// Al enviarse el formulario, se debe ejecutar un PUT al servidor
-// 	$("#formCompetencia").ajaxForm({url: server + '/competencias/' + idCompetencia, type: 'put', 
-// 		// En caso de éxito, se redirige a index.html
-// 		success: function(res) {
-// 			window.location.replace("./index.html?exito=True");
-// 		},
-// 		// En caso de error por validación, se muestra el mensaje de error en el contenedor para tal fin
-// 		error: function(response, status, xhr) {
-// 			if (response.status == 422){
-// 				$("#mensajeDeError").text(response.responseText);
-// 			}
-// 		}
-// 	});
-// 	// Si el usuario cancela, se redirige a index.html
-// 	$(".cancelar").click(function(){
-// 		window.location.replace("./index.html");
-// 	});
-// });
-
-
-// $('#new').click(function(event){
-//     event.preventDefault();
-//     var server = 'http://localhost:8080';
-//     var name = $('#exampleInputName1').val();
-//     var email = $('#exampleInputEmail1').val();
-//     var password = $('#exampleInputPassword1').val();
-    
-
-//     // si uso $.post y no hace falta especificar el metodo
-//     $.ajax({
-//         url: server + "/usuario/crear",
-//         method: 'post',
-//         data: {"nombre": name, "email": email, 'password': password},
-//         success: function(){
-//             alert('Usuario creado con exito');
-//             window.location.href = "index.html";
-//         },
-//         error: function(error){
-//             window.location.href = "error.html";
-//         }
-//     });
-// });
